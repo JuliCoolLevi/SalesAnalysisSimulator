a6f8858ff38eef54437e9308a203871c75dc39a6
Julius Nillo <nilloj@sheridancollege.ca>
2025-09-03 00:07:54 -0400

    Initial Commit

---
+++ SalesAnalysisSimulator/SalesAnalysisSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesAnalysisSimulatorApp.swift
+//  SalesAnalysisSimulator
+//
+//  Created by Julius Nillo on 2025-09-03.
+//
+
+import SwiftUI
+
+@main
+struct SalesAnalysisSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
