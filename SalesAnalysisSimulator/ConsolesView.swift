71aa9232f478275c8e7dd2f177b74d81838a354c
Julius Nillo <nilloj@sheridancollege.ca>
2025-09-03 12:49:05 -0400

    Made navigation buttons (will plan on making filters)

---
+++ SalesAnalysisSimulator/ConsolesView.swift
@@ -0,0 +1,18 @@
+//
+//  ConsolesView.swift
+//  SalesAnalysisSimulator
+//
+//  Created by Julius Nillo on 2025-09-03.
+//
+
+import SwiftUI
+
+struct ConsolesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConsolesView()
+}
---
+++ SalesAnalysisSimulator/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SalesAnalysisSimulator
+//
+//  Created by Julius Nillo on 2025-09-03.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
